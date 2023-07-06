c2817973cf847d832a61df81d1fa30b01ce9ef58
CENSOR_1337 <viixxivvimm@gmail.com>
2023-07-07 06:19:29 +0700

    feat: add `isServer`, `isClient` export

--- src/shared/resource/index.ts
+++ src/shared/resource/index.ts
@@ -4,6 +4,9 @@ export const getResourceEventName = (event: string) => {
 	return `${resourceName}:${event}`;
 };
 
+export const isServer = IsDuplicityVersion();
+export const isClient = !isServer;
+
 export const onResourceStop = (callback: () => void) => {
 	return on("onResourceStop", (resource: string) => {
 		if (resource !== resourceName) return;
