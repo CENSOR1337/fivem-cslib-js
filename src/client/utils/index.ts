c88f3565cdc54d21a172023fb72f9810ddfda119
CENSOR_1337 <viixxivvimm@gmail.com>
2023-07-07 17:52:47 +0700

    feat: add randomUUID on client

--- src/client/utils/index.ts
+++ src/client/utils/index.ts
@@ -6,3 +6,27 @@ const cfx = {
 export function emitServer(eventName: string, ...args: any[]): void {
 	return cfx.triggerServerEvent(eventName, ...args);
 }
+
+export function randomUUID(): string {
+	let uuid = "";
+	for (let ii = 0; ii < 32; ii += 1) {
+		switch (ii) {
+			case 8:
+			case 20:
+				uuid += "-";
+				uuid += ((Math.random() * 16) | 0).toString(16);
+				break;
+			case 12:
+				uuid += "-";
+				uuid += "4";
+				break;
+			case 16:
+				uuid += "-";
+				uuid += ((Math.random() * 4) | 8).toString(16);
+				break;
+			default:
+				uuid += ((Math.random() * 16) | 0).toString(16);
+		}
+	}
+	return uuid;
+}
